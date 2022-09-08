9b5a1a6d55376b80c2a8c13f9c5813aa53e87653
РИШАТ Ахтямов <rishat-rus@mail.ru>
2022-09-08 21:41:50 +0400

    coment

--- SF2022User12Lib/Calculations.cs
+++ SF2022User12Lib/Calculations.cs
@@ -34,68 +34,69 @@ namespace SF2022User12Lib
 
             #endregion
 
-            TimeSpan interval = new TimeSpan(0, consultationTime, 0);
+            TimeSpan interval = new TimeSpan(0, consultationTime, 0); // consultationTime В TimeSpan
 
-            List<TimeSpan> startedIterator = new List<TimeSpan>() ;
+            List<TimeSpan> startedIterator = new List<TimeSpan>() ; // времена  начала интервалов 
+            // например 8-00 иди 11-30 
 
-            if (beginWorkingTime + interval <= startTimes.First())
+            if (beginWorkingTime + interval <= startTimes.First()) //проверка помещается  ли начало  работы в  интервал 
                 startedIterator.Add(beginWorkingTime);
 
-
-            for (int i = 0; i < startTimes.Length -1 ; i++)
+            for (int i = 0; i < startTimes.Length -1 ; i++) // перебираем массив  startTimes кроме последней 
             {
-               
-                var  tim = startTimes[i]  + new TimeSpan( 0, durations[i], 0);
-                if (tim + interval <= startTimes[i + 1])
+                var  tim = startTimes[i]  + new TimeSpan( 0, durations[i], 0); // конец интервала например 10-00 +60  ->11-00
+                if (tim + interval <= startTimes[i + 1]) // если между остановками помещается интервал значит это  и есть начало работы 
                 {
-                    startedIterator.Add(tim);
+                    startedIterator.Add(tim); // добавим  в  список начал  работы
                 }
             }
 
-            var last = startTimes.Last() + new TimeSpan(0, durations.Last(), 0);
+            var last = startTimes.Last() + new TimeSpan(0, durations.Last(), 0); // найдем  последнию остановку
 
-            if (last + interval <= endWorkingTime)
-                startedIterator.Add(last);
+            if (last + interval <= endWorkingTime) // если  после остановки  достаточно  времяни  до  конца раб дня
+                startedIterator.Add(last); // тоже  добавим  в  наш список 
 
+            List<TimeSpan> tryTime = new List<TimeSpan>() ; // сюда будем складывать начала наших искомых  отрезков
 
-            List<TimeSpan> tryTime = new List<TimeSpan>() ;
-
-
-            foreach (var item in startedIterator)
+            foreach (var st in startedIterator) // переберем наши интервалы -- те точки  откуда начинаем работу 
             {
-                TimeSpan stop=item;
-                foreach (var t  in startTimes)
+                TimeSpan stop=st; // ищим крайнюю точку  в наших мини-отрезках
+                foreach (var t  in startTimes) // перебирам точки  остановок 
                 {
-                    if(item <= t)
+                    if(st <= t) // и  ищем  ближайшую   к старту
                     {
-                        stop = t;
+                        stop = t; // нашли точку остановки
                         break;
                     }
 
                     if(t == startTimes.Last())
                     {
-                        stop = endWorkingTime;
+                        stop = endWorkingTime; // смотрим   является ли конец рабочего дня крайней точкой работы
                     }
                 }
 
-                tryTime.AddRange(GenereticDate(item, stop, interval));
+                tryTime.AddRange(GenereticDate(st, stop, interval)); // ищим массив наших интервалов
             }
 
-            string [] res = new string [tryTime.Count] ;
+            string [] resultat = new string [tryTime.Count] ; // переведем  в  строчное  представление 
 
             for (int i = 0; i < tryTime.Count; i++)
             {
-                var next = tryTime[i]+interval ;
-               
-                             
-                res[i] = $"{MyPars( tryTime[i].Hours.ToString())}" +
-                    $":{MyPars(tryTime[i].Minutes.ToString())}-{MyPars(next.Hours.ToString())}" +
-                    $":{MyPars(next.Minutes.ToString())}";
+                var next = tryTime[i]+interval ; // конечная точка сектора 8:00-8:30
+
+                   resultat[i] = $"{MyPars( tryTime[i].Hours.ToString())}" +
+                   $":{MyPars(tryTime[i].Minutes.ToString())}-{MyPars(next.Hours.ToString())}" +
+                   $":{MyPars(next.Minutes.ToString())}"; // Собираем  в  искомов  формате
             }
 
-            return res; 
+            return resultat; 
         }
-
+        
+        /// <summary>
+        /// добавляет   0 в строке  в  1 символ - например 9 будет  как 09
+        /// </summary>
+        /// <param name="sumbol"></param>
+        /// <returns></returns>
         private string MyPars(string sumbol)
         {
             if (sumbol.Length == 1)
@@ -104,15 +105,23 @@ namespace SF2022User12Lib
                 return sumbol;
         }
 
-        private List<TimeSpan> GenereticDate(TimeSpan item, TimeSpan stop, TimeSpan interval)
-        {
-            List<TimeSpan> result = new List<TimeSpan>() ;
-            var s = item;
 
-            while (s+interval<=stop)
+        /// <summary>
+        /// Генерирует  все точки  интервалов  от  старта работы  до  окончания  по  интервалу 
+        /// </summary>
+        /// <param name="start">Например 8-00</param>
+        /// <param name="stop">Например 10-00</param>
+        /// <param name="interval">например 30 мину </param>
+        /// <returns>8-00/8-30/9-00/9-30</returns>
+        private List<TimeSpan> GenereticDate(TimeSpan start, TimeSpan stop, TimeSpan interval)
+        {
+            List<TimeSpan> result = new List<TimeSpan>() ; // сюда будем  складывать  
+            var s = start; // сохраним стартовое  значение 
+
+            while (s+interval<=stop) 
             {
-                result.Add(s);
-                s+=interval;
+                result.Add(s); // добавим значние
+                s+=interval; 
             }
             return result;  
         }
