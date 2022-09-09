25d4bbb75979ee456a605a0a02a66dc31cecf13d
Ahtyamov <Ahtyamov@WIN-SFUQ8BAFB06>
2022-09-09 08:39:41 +0300

    отредачил код

--- SF2022User12Lib/Calculations.cs
+++ SF2022User12Lib/Calculations.cs
@@ -36,15 +36,24 @@ namespace SF2022User12Lib
 
             TimeSpan interval = new TimeSpan(0, consultationTime, 0); // consultationTime В TimeSpan
 
-            List<TimeSpan> startedIterator = new List<TimeSpan>() ; // времена  начала интервалов 
+            List<TimeSpan> startedIterator = NewMetSearchForStartPointhod(startTimes, durations, beginWorkingTime, endWorkingTime, interval);
+            List<TimeSpan> tryTime = TryTimeintervals(startTimes, endWorkingTime, interval, startedIterator);
+            string[] resultat = TimeSpanToStringArrya(interval, tryTime);
+
+            return resultat;
+        }
+
+        private static List<TimeSpan> NewMetSearchForStartPointhod(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, TimeSpan interval)
+        {
+            List<TimeSpan> startedIterator = new List<TimeSpan>(); // времена  начала интервалов 
             // например 8-00 иди 11-30 
 
             if (beginWorkingTime + interval <= startTimes.First()) //проверка помещается  ли начало  работы в  интервал 
                 startedIterator.Add(beginWorkingTime);
 
-            for (int i = 0; i < startTimes.Length -1 ; i++) // перебираем массив  startTimes кроме последней 
+            for (int i = 0; i < startTimes.Length - 1; i++) // перебираем массив  startTimes кроме последней 
             {
-                var  tim = startTimes[i]  + new TimeSpan( 0, durations[i], 0); // конец интервала например 10-00 +60  ->11-00
+                var tim = startTimes[i] + new TimeSpan(0, durations[i], 0); // конец интервала например 10-00 +60  ->11-00
                 if (tim + interval <= startTimes[i + 1]) // если между остановками помещается интервал значит это  и есть начало работы 
                 {
                     startedIterator.Add(tim); // добавим  в  список начал  работы
@@ -52,46 +61,51 @@ namespace SF2022User12Lib
             }
 
             var last = startTimes.Last() + new TimeSpan(0, durations.Last(), 0); // найдем  последнию остановку
-
             if (last + interval <= endWorkingTime) // если  после остановки  достаточно  времяни  до  конца раб дня
                 startedIterator.Add(last); // тоже  добавим  в  наш список 
+            return startedIterator;
+        }
 
-            List<TimeSpan> tryTime = new List<TimeSpan>() ; // сюда будем складывать начала наших искомых  отрезков
-
+        private List<TimeSpan> TryTimeintervals(TimeSpan[] startTimes, TimeSpan endWorkingTime, TimeSpan interval, List<TimeSpan> startedIterator)
+        {
+            List<TimeSpan> tryTime = new List<TimeSpan>(); // сюда будем складывать начала наших искомых  отрезков
             foreach (var st in startedIterator) // переберем наши интервалы -- те точки  откуда начинаем работу 
             {
-                TimeSpan stop=st; // ищим крайнюю точку  в наших мини-отрезках
-                foreach (var t  in startTimes) // перебирам точки  остановок 
+                TimeSpan stop = st; // ищим крайнюю точку  в наших мини-отрезках
+                foreach (var t in startTimes) // перебирам точки  остановок 
                 {
-                    if(st <= t) // и  ищем  ближайшую   к старту
+                    if (st <= t) // и  ищем  ближайшую   к старту
                     {
                         stop = t; // нашли точку остановки
                         break;
                     }
 
-                    if(t == startTimes.Last())
+                    if (t == startTimes.Last())
                     {
                         stop = endWorkingTime; // смотрим   является ли конец рабочего дня крайней точкой работы
                     }
                 }
-
                 tryTime.AddRange(GenereticDate(st, stop, interval)); // ищим массив наших интервалов
             }
+            return tryTime;
+        }
 
-            string [] resultat = new string [tryTime.Count] ; // переведем  в  строчное  представление 
+        private string[] TimeSpanToStringArrya(TimeSpan interval, List<TimeSpan> tryTime)
+        {
+            string[] resultat = new string[tryTime.Count]; // переведем  в  строчное  представление 
 
             for (int i = 0; i < tryTime.Count; i++)
             {
-                var next = tryTime[i]+interval ; // конечная точка сектора 8:00-8:30
+                var next = tryTime[i] + interval; // конечная точка сектора 8:00-8:30
 
-                   resultat[i] = $"{MyPars( tryTime[i].Hours.ToString())}" +
-                   $":{MyPars(tryTime[i].Minutes.ToString())}-{MyPars(next.Hours.ToString())}" +
-                   $":{MyPars(next.Minutes.ToString())}"; // Собираем  в  искомов  формате
+                resultat[i] = $"{MyPars(tryTime[i].Hours.ToString())}" +
+                $":{MyPars(tryTime[i].Minutes.ToString())}-{MyPars(next.Hours.ToString())}" +
+                $":{MyPars(next.Minutes.ToString())}"; // Собираем  в  искомов  формате
             }
 
-            return resultat; 
+            return resultat;
         }
-        
+
         /// <summary>
         /// добавляет   0 в строке  в  1 символ - например 9 будет  как 09
         /// </summary>
